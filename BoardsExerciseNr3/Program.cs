bfe15d5cafa8f0d645cfc19d4215712d4ccff9f9
konrad101296 <konradslimak@o2.pl>
2024-03-16 19:09:40 +0100

    Add project files.

---
+++ BoardsExerciseNr3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Numerics;
+
+int[] board = new int[10];
+Random random = new Random();
+for (int i = 0; i < 10; i++)
+{
+    board[i] = random.Next();
+    Console.WriteLine($"the {i + 1} element one the board is:" + board[i]);
+}
+int biggest = 0;
+int smallest = Int32.MaxValue;
+int biggestIndex = 0;
+int smallestIndex = 0;
+for (int i = 0; i < 10; i++)
+{
+    if (biggest < board[i])
+    {
+        biggest = board[i];
+        biggestIndex = i + 1;
+    }
+    if (smallest > board[i])
+    {
+        smallest = board[i];
+        smallestIndex = i + 1;
+    }
+}
+Console.WriteLine($"the smallest value has index {smallestIndex} and is {smallest}");
+Console.WriteLine($"the biggest value is {biggestIndex} and is {biggest}");
